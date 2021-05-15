57deeb5848ea2c9bb672dcf2a999636e99885d80
Maher <ma.meer@hotmail.com>
2021-05-15 22:26:07 +0200

    edit distinct statments

--- Week2/homework/4-aggredate-functions.js
+++ Week2/homework/4-aggredate-functions.js
@@ -8,13 +8,13 @@ import {
 //
 const queries = [
   `SELECT 
-     paper_title, COUNT(author_no) AS authors
+     paper_title, COUNT(DISTINCT author_no) AS authors
      FROM research_papers
      JOIN author_paper
      ON research_papers.paper_id = author_paper.paper_id 
      GROUP BY author_paper.paper_id`,
-  `SELECT DISTINCT
-     COUNT(*), gender
+  `SELECT
+     COUNT(DISTINCT paper_id) As written_by_female, gender
      FROM author_paper
      JOIN authors 
      ON author_paper.author_no = authors.author_no 
@@ -23,8 +23,8 @@ const queries = [
      university, AVG(h_index)
      FROM authors
      GROUP BY authors.university`,
-  `SELECT DISTINCT
-     university, COUNT(paper_id ) 
+  `SELECT
+     university, COUNT(DISTINCT paper_id) AS total_papers
      FROM authors
      JOIN author_paper
      ON authors.author_no = author_paper.author_no
